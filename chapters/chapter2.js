9024a15a3c26e04d68481194480521715b01a335
M-wasi123 <wasikhatri11@gmail.com>
2024-03-17 09:15:59 +0500

    Update chapter2.js

--- chapters/chapter2.js
+++ chapters/chapter2.js
@@ -1,2 +1,18 @@
-var userName = 'Jhone Doe'
-alert(userName)
+ var userName = 'Jhone Doe'
+ alert("                                               " +userName)
+ var userAge = 21 + ' years old'
+ alert("                                          " + userAge)
+ var userText = "Certified Mobile Application Development"
+ alert("                          " + userText)
+ var userData = "pizza"
+ var userData1 = "pizz"
+ var userData2 = "piz"
+ var userData3 = "pi"
+ var userData4 = "p"
+ alert("                                   " + userData + "\n"+"                                   " + userData1 + "\n"+"                                   " + userData2 + "\n"+"                                   " + userData3 + "\n"+"                                   " + userData4)
+ var userEmail = 'My email address is example@example.com'
+ alert("              "+  userEmail)
+ var userBook = 'I am trying to learn from the Book A Smarter \n           Way to learn JavaScript'
+ alert("           "+  userBook)
+var userDesign = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert('             ' + userDesign)
