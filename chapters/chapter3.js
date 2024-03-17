3005716a85e9e46d220506c9620cfd308574373a
M-wasi123 <wasikhatri11@gmail.com>
2024-03-17 10:16:12 +0500

    new

--- chapters/chapter3.js
+++ chapters/chapter3.js
@@ -1,3 +1,9 @@
-var name1 = document.getElementById('heading').innerHTML;
-
-document.write(name1)
+document.getElementById('heading').innerHTML = (`<span style="display: flex; align-items: center;
+gap: 5px;">
+<h3 >John Doe</h3>
+<p>ordered</p>
+<span style="display: flex;align-items: center;">
+<h3>5 T-Shirt</h3>
+<p>(s) on XYZ Clothing store</p>
+</span>
+</span>`);
