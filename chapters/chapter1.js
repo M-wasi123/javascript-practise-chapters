2631cd03c55b0e222ab59952ae649cb319ca1ecc
M-wasi123 <wasikhatri11@gmail.com>
2024-03-17 08:46:19 +0500

    new

---
+++ chapters/chapter1.js
@@ -0,0 +1,7 @@
+alert('Error! Please enter a valid password')
+alert('Welcome to Js Land...\nHappy Coding!')
+alert('Welcome to JS Land...')
+alert('Happy Coding!\nPrevent this page from creating additional dialogs.' )
+alert("Hello... I can run JS through my web browser's console")
+
+
---
+++ chapters/chapter2.js
@@ -0,0 +1,2 @@
+var userName = 'Jhone Doe'
+alert(userName)
