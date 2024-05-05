0967ad2d84da3a640abb94dd5fce51095cdbdea3
M-wasi123 <wasikhatri11@gmail.com>
2024-05-05 13:38:20 +0500

    Update chapter11.js

--- chapters/chapter11.js
+++ chapters/chapter11.js
@@ -21,3 +21,10 @@ document.write('Current Day: '+ currentDay + '<br><br>')
 // 5. Write a program that shows the message “First fifteen
 // days of the month” if the date is less than 16th of the month
 // else shows “Last days of the month”.
+
+let currentDate = new Date().getDate();
+if (currentDate < 16) {
+    document.write('First fifteen days of the month')
+}else{
+    document.write('Last days of the month')
+}
