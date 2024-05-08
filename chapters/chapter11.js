b322fa9a5e11206442da13d357c2f4e97e56f4ed
M-wasi123 <wasikhatri11@gmail.com>
2024-05-08 07:33:51 +0500

    Update chapter11.js

--- chapters/chapter11.js
+++ chapters/chapter11.js
@@ -48,8 +48,50 @@ document.write('Elapsed minutes since January 1 , 1970: '+ minutes +'<br><br>')
 
 let userTime = new Date().getHours();
 if (userTime < 13) {
-    document.write("It's AM")
+    document.write("It's AM"+'<br><br>')
 } else {
-    document.write("It's PM") 
+    document.write("It's PM"+'<br><br>') 
 }
+
+// 8. Write a program that creates a Date object for the last day
+// of the last month of 2020 and assigns it to variable named
+// laterDate.
+
+let laterDate = new Date('2020-12-31T00:00:00')
+document.write('Later Date: ' + laterDate +'<br><br>')
+
+// 10. Write a program that displays in your browser the
+// seconds that elapsed between the reference date and the
+// beginning of 2015.
+
+let referenceDate = new Date('2015-12-05T22:50:16');
+let beginningDate = new Date('2015-01-01T00:00:00');
+let referenceDate1 = new Date('2015-12-05T22:50:16').getTime();
+let beginningDate1 = new Date('2015-12-05T00:00:00').getTime();
+let since = referenceDate1 - beginningDate1
+let sinceDifference = since
+document.write('On refrence date '+referenceDate +'<br><br>');
+document.write(sinceDifference + ' Seconds had passed since begining of 2015');
+
+
+// 9. Create a date object of the starting date of this Ramadan
+// and alert the number of days past since 1st Ramadan?
+// Note: 1st Ramadan was on June 18, 2015
+
+
+let ramzanWait = new Date('2015-01-01').getMonth();
+let ramzanMonth = new Date('2015-06-18').getMonth();
+let ramzanDate = new Date('2015-06-18').getDate();
+let ramzanDifference = ramzanMonth - ramzanWait;
+let difrence = ramzanDifference * 30 + 21;
+let ramzan;
+ramzan.setInterval(() => {
+    difrence--;
+    alert(difrence + 'Days have passed since 1st Ramzan, 2015')
+    if (difrence === 0) {
+        alert('Today is first RAMZAN')
+    }
+},5184000000 );
+
+
 
