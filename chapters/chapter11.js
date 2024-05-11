72dc2e0b95e4ed29575a75da865f7924ac26412c
M-wasi123 <wasikhatri11@gmail.com>
2024-05-11 06:53:40 +0500

    Update chapter11.js

--- chapters/chapter11.js
+++ chapters/chapter11.js
@@ -65,14 +65,62 @@ document.write('Later Date: ' + laterDate +'<br><br>')
 // beginning of 2015.
 
 let referenceDate = new Date('2015-12-05T22:50:16');
-let beginningDate = new Date('2015-01-01T00:00:00');
 let referenceDate1 = new Date('2015-12-05T22:50:16').getTime();
-let beginningDate1 = new Date('2015-12-05T00:00:00').getTime();
+let beginningDate1 = new Date('2015-01-05T00:00:00').getTime();
 let since = referenceDate1 - beginningDate1
-let sinceDifference = since
+let sinceDifference = since / 60
 document.write('On refrence date '+referenceDate +'<br><br>');
-document.write(sinceDifference + ' Seconds had passed since begining of 2015');
+document.write(sinceDifference.toFixed(0) + ' Seconds had passed since begining of 2015'+'<br><br>');
 
+
+// 11. Create a Date object for the current date and time.
+// Extract the hours, reset the date object an hour ahead and
+// finally display the date object in your browser.
+
+let currenDate = new Date('2015-12-05T23:08:16')
+let hoursAgo = new Date('2015-12-05T22:08:16')
+
+document.write('Current Date: '+ currenDate+'<br><br>')
+document.write('1 hour ago, it was '+hoursAgo+'<br><br>')
+
+// 12. Write a program that creates a date object and show the
+// date in an alert box that is reset to 100 years back?
+
+let curenDate = new Date('2015-12-05T23:09:37')
+let yearsBack = new Date('1915-12-05T23:09:37')
+
+document.write('Current Date: '+ curenDate+'<br><br>')
+document.write('100 years back, it was '+yearsBack+'<br><br>')
+
+// 13. Write a program to ask the user about his age. Calculate
+// and show his birth year in your browser.
+
+let userAge = +prompt('Enter your age');
+let userDate = new Date().getFullYear();
+let birthYear = userDate - userAge;
+document.write('Your age is '+userAge +'<br><br>'+ 'Your birth year is '+ birthYear +'<br><br>')
+
+// 14. Write a program to generate your K-Electric bill in your
+// browser. All the amounts should be rounded off to 2
+// decimal places. Display the following fields:
+// a. Customer Name
+// b. Current Month
+// c. Number of units
+// d. Charges per unit
+// e. Net Amount Payable (within Due Date)
+// f. Late Payment Surcharge
+// g. Gross Amount Payable (after Due Date)
+// Where,
+
+let customerName = 'ABC Customer'
+let curentMonth = new Date().getMonth();
+let NumberOfUnits = 410;
+let chargesPerUnit = 16;
+let amountPaybale = NumberOfUnits*chargesPerUnit;
+let latePayment = 350;
+let surCharge = amountPaybale+latePayment;
+
+document.write('<h1>K-Electric Bill</h1>'+'<br><br><br><br>'+"Customer Name: "+customerName +'<br><br>'+'Current Month: '+currentMonth+'<br><br>'+'Number of units: '+NumberOfUnits +'<br><br>'+'Charges per unit: '+chargesPerUnit + '<br><br>' +'Net Amount Payable (within Due Date): '+amountPaybale +'<br><br>'+ 'Gross Amount Payable (after Due Date): '+ surCharge)
 
 // 9. Create a date object of the starting date of this Ramadan
 // and alert the number of days past since 1st Ramadan?
@@ -92,6 +140,4 @@ ramzan.setInterval(() => {
         alert('Today is first RAMZAN')
     }
 },5184000000 );
-
-
 
